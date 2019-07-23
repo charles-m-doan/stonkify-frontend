bcec7c73a7c45015036fbe429b9f3ea3fb2aa805
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-23 14:59:29 -0400

    added artistsView data from api

--- src/js/Api/Api.js
+++ src/js/Api/Api.js
@@ -1,16 +1,12 @@
 export default function() {
-	return new Api();
+  return new Api();
 }
 
-class Api {}
-
-// -------------- DONNIE's CODE BELOW ----------------
-
-// class Api {
-//     getRequest(location, callback) {
-//       fetch(location)
-//         .then(response => response.json())
-//         .then(callback)
-//         .catch(err => console.log(err));
-//     }
-//   }
+class Api {
+  getRequest(location, callback) {
+    fetch(location)
+      .then(response => response.json())
+      .then(callback)
+      .catch(err => console.log(err));
+  }
+}
--- src/js/Components/ArtistsViewComponents.js
+++ src/js/Components/ArtistsViewComponents.js
@@ -24,37 +24,36 @@ class ArtistsViewComponents {
       .create("section")
       .addClass("card-gallery");
 
-    const artistCollection = [0, 0, 0, 0, 0, 0, 0];
-    artistCollection.forEach(artist => {
-      const card = Html()
-        .create("article")
-        .addClass("card");
+    // let artistCollection = [0, 0, 0, 0, 0, 0, 0];
+    // let artistCollection;
+    Api().getRequest(`http://localhost:8080/api/artists`, artistCollection => {
+      artistCollection.forEach(artist => {
+        const card = Html()
+          .create("article")
+          .addClass("card");
 
-      const cardFigure = Html()
-        .create("figure")
-        .addClass("card__figure");
+        const cardFigure = Html()
+          .create("figure")
+          .addClass("card__figure");
 
-      const cardFigurePortrait = Html()
-        .create("img")
-        .addClass("card__figure__portrait")
-        .addAttribute(
-          "src",
-          "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxODMyODA0ODc3/frank-zappa-9540382-1-402.jpg"
-        );
+        const cardFigurePortrait = Html()
+          .create("img")
+          .addClass("card__figure__portrait")
+          .addAttribute("src", artist.imgUrl);
 
-      const cardTitle = Html()
-        .create("h3")
-        .addClass("card__title")
-        .text("Frank Zappa");
+        const cardTitle = Html()
+          .create("h3")
+          .addClass("card__title")
+          .text(artist.name);
 
-      card.addChild(cardFigure);
-      cardFigure.addChild(cardFigurePortrait);
-      card.addChild(cardTitle);
-      cardGallery.addChild(card);
+        card.addChild(cardFigure);
+        cardFigure.addChild(cardFigurePortrait);
+        card.addChild(cardTitle);
+        cardGallery.addChild(card);
+      });
     });
 
     main.addChild(pageTitle);
     main.addChild(cardGallery);
-    
   }
 }
