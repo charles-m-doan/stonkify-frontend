4f06a3ebaefa70576fa6962329940f9d46d15d7c
KaiHennacy <kchennacy@gmail.com>
2019-07-24 11:07:38 -0400

    Renders single album page in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import SkeletonViewComponents from "./Components/SkeletonViewComponents";
 import SingleArtistViewComponents from "./Components/SingleArtistViewComponents";
 import ArtistsViewComponents from "./Components/ArtistsViewComponents";
+import SingleAlbumViewComponents from "./Components/SingleAlbumViewComponents";
 
 import Api from "./Api/Api";
 
-SkeletonViewComponents().renderSkeletonView();
+// SkeletonViewComponents().renderSkeletonView();
 // SingleArtistViewComponents().renderSingleArtistView();
 // ArtistsViewComponents().renderArtistsView();
+SingleAlbumViewComponents().renderSingleAlbumView();
