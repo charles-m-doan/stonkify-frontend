1c721b9ede658e35760cbc695a7e87ef7caa9ca1
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-25 13:14:10 -0400

    Finishing basic content block classes.

--- src/js/Components/ComponentManager.js
+++ src/js/Components/ComponentManager.js
@@ -8,199 +8,199 @@ import SingleArtistBlock from "../Components/ContentBlocks/SingleArtistBlock";
 import SingleAlbumBlock from "../Components/ContentBlocks/SingleAlbumBlock";
 
 export default class ComponentManager {
-	constructor() {
-		this.skeletonHasBeenRendered = false;
-		this.blockTypes = [
-			"unknownBlock",
-			"homeBlock",
-			"allArtistsBlock",
-			"allAlbumsBlock",
-			"singleArtistBlock",
-			"singleAlbumBlock"
-		];
-	}
+  constructor() {
+    this.skeletonHasBeenRendered = false;
+    this.blockTypes = [
+      "unknownBlock",
+      "homeBlock",
+      "allArtistsBlock",
+      "allAlbumsBlock",
+      "singleArtistBlock",
+      "singleAlbumBlock"
+    ];
+  }
 
-	getAppContext() {
-		return Html().select("#app");
-	}
+  getAppContext() {
+    return Html().select("#app");
+  }
 
-	renderContentBlock(blockType, requestedData) {
-		if (this.skeletonHasBeenRendered == false) {
-			this.renderPageSkeleton();
-		}
-		if (blockType === undefined) {
-			blockType = "homeBlock";
-		}
+  renderContentBlock(blockType, requestedData) {
+    if (this.skeletonHasBeenRendered == false) {
+      this.renderPageSkeleton();
+    }
+    if (blockType === undefined) {
+      blockType = "homeBlock";
+    }
 
-		// Branch Based on Block Type
-		if (blockType == "homeBlock") {
-			const block = new HomeBlock(requestedData);
-			block.renderContent();
-		} else if (blockType == "allArtistsBlock") {
-			const block = new AllArtistsBlock(requestedData);
-			block.renderContent();
-		} else if (blockType == "allAlbumsBlock") {
-			const block = new AllAlbumsBlock(requestedData);
-			block.renderContent();
-		} else if (blockType == "singleArtistBlock") {
-			const block = new SingleArtistBlock(requestedData);
-			block.renderContent();
-		} else if (blockType == "singleAlbumBlock") {
-			const block = new SingleAlbumBlock(requestedData);
-			block.renderContent();
-		} else {
-			const unknownBlock = new UnknownBlock();
-			unknownBlock.renderContent();
-		}
-	}
+    // Branch Based on Block Type
+    if (blockType == "homeBlock") {
+      const block = new HomeBlock(requestedData);
+      block.renderContent();
+    } else if (blockType == "allArtistsBlock") {
+      const block = new AllArtistsBlock(requestedData);
+      block.renderContent();
+    } else if (blockType == "allAlbumsBlock") {
+      const block = new AllAlbumsBlock(requestedData);
+      block.renderContent();
+    } else if (blockType == "singleArtistBlock") {
+      const block = new SingleArtistBlock(requestedData);
+      block.renderContent();
+    } else if (blockType == "singleAlbumBlock") {
+      const block = new SingleAlbumBlock(requestedData);
+      block.renderContent();
+    } else {
+      const unknownBlock = new UnknownBlock();
+      unknownBlock.renderContent();
+    }
+  }
 
-	renderPageSkeleton() {
-		if (this.skeletonHasBeenRendered == false) {
-			// console.log("Rendering Page Skeleton!");
+  renderPageSkeleton() {
+    if (this.skeletonHasBeenRendered == false) {
+      // console.log("Rendering Page Skeleton!");
 
-			this.renderWrapper();
-			this.renderSidebar();
-			this.renderEmptyContentPane();
-			this.renderMain();
-			this.renderFooter();
+      this.renderWrapper();
+      this.renderSidebar();
+      this.renderEmptyContentPane();
+      this.renderMain();
+      this.renderFooter();
 
-			this.skeletonHasBeenRendered = true;
-		} else {
-			console.log("Page Skeleton has already been rendered!");
-		}
-	}
+      this.skeletonHasBeenRendered = true;
+    } else {
+      console.log("Page Skeleton has already been rendered!");
+    }
+  }
 
-	renderWrapper() {
-		const app = Html().select("#app");
-		const wrapper = Html()
-			.create("div")
-			.addClass("wrapper");
-		app.addChild(wrapper);
-	}
+  renderWrapper() {
+    const app = Html().select("#app");
+    const wrapper = Html()
+      .create("div")
+      .addClass("wrapper");
+    app.addChild(wrapper);
+  }
 
-	renderSidebar() {
-		const wrapper = Html().select(".wrapper");
+  renderSidebar() {
+    const wrapper = Html().select(".wrapper");
 
-		const banner = Html()
-			.create("section")
-			.addClass("banner")
-			.addClass("sidebar");
-		const bannerLogo = Html()
-			.create("div")
-			.addClass("banner__logo");
-		const bannerLogoFigure = Html()
-			.create("figure")
-			.addClass("banner__logo__image");
-		const figureImg = Html()
-			.create("img")
-			.addAttribute("src", logoAvatar)
-			.addAttribute("alt", "Meme Man");
+    const banner = Html()
+      .create("section")
+      .addClass("banner")
+      .addClass("sidebar");
+    const bannerLogo = Html()
+      .create("div")
+      .addClass("banner__logo");
+    const bannerLogoFigure = Html()
+      .create("figure")
+      .addClass("banner__logo__image");
+    const figureImg = Html()
+      .create("img")
+      .addAttribute("src", logoAvatar)
+      .addAttribute("alt", "Meme Man");
 
-		const bannerLogoTitle = Html()
-			.create("h1")
-			.addClass("banner__logo__title")
-			.text("Stonkify");
+    const bannerLogoTitle = Html()
+      .create("h1")
+      .addClass("banner__logo__title")
+      .text("Stonkify");
 
-		const bannerNavList = Html()
-			.create("ul")
-			.addClass("banner__nav-list");
+    const bannerNavList = Html()
+      .create("ul")
+      .addClass("banner__nav-list");
 
-		//HOME BUTTON
+    //HOME BUTTON
 
-		const homeViewItem = Html()
-			.create("li")
-			.addClass("nav-list__item");
-		homeViewItem.addChild(
-			Html()
-				.create("a")
-				.addClass("nav-list__anchor")
-				.addAttribute("href", "homeBlock")
-				.text("Home")
-				.click(event => {
-					event.preventDefault();
-					// console.log("Home clicked!");
-					this.renderContentBlock("homeBlock", "");
-				})
-		);
+    const homeViewItem = Html()
+      .create("li")
+      .addClass("nav-list__item");
+    homeViewItem.addChild(
+      Html()
+        .create("a")
+        .addClass("nav-list__anchor")
+        .addAttribute("href", "homeBlock")
+        .text("Home")
+        .click(event => {
+          event.preventDefault();
+          // console.log("Home clicked!");
+          this.renderContentBlock("homeBlock", "");
+        })
+    );
 
-		//ARTISTS BUTTON
+    //ARTISTS BUTTON
 
-		const artistsViewItem = Html()
-			.create("li")
-			.addClass("nav-list__item");
-		artistsViewItem.addChild(
-			Html()
-				.create("a")
-				.addClass("nav-list__anchor")
-				.addAttribute("href", "allArtistsBlock")
-				.text("Artists")
-				.click(event => {
-					event.preventDefault();
-					// console.log("Artists clicked!");
-					this.renderContentBlock("allArtistsBlock", "");
-				})
-		);
+    const artistsViewItem = Html()
+      .create("li")
+      .addClass("nav-list__item");
+    artistsViewItem.addChild(
+      Html()
+        .create("a")
+        .addClass("nav-list__anchor")
+        .addAttribute("href", "allArtistsBlock")
+        .text("Artists")
+        .click(event => {
+          event.preventDefault();
+          // console.log("Artists clicked!");
+          this.renderContentBlock("allArtistsBlock", "");
+        })
+    );
 
-		//ALBUMS BUTTON
+    //ALBUMS BUTTON
 
-		const albumsViewItem = Html()
-			.create("li")
-			.addClass("nav-list__item");
-		albumsViewItem.addChild(
-			Html()
-				.create("a")
-				.addClass("nav-list__anchor")
-				.addAttribute("href", "allAlbumsBlock")
-				.text("Albums")
-				.click(event => {
-					event.preventDefault();
-					// console.log("Albums clicked!");
-					this.renderContentBlock("allAlbumsBlock", "");
-				})
-		);
+    const albumsViewItem = Html()
+      .create("li")
+      .addClass("nav-list__item");
+    albumsViewItem.addChild(
+      Html()
+        .create("a")
+        .addClass("nav-list__anchor")
+        .addAttribute("href", "allAlbumsBlock")
+        .text("Albums")
+        .click(event => {
+          event.preventDefault();
+          // console.log("Albums clicked!");
+          this.renderContentBlock("allAlbumsBlock", "");
+        })
+    );
 
-		bannerLogoFigure.addChild(figureImg);
-		bannerLogo.addChild(bannerLogoFigure);
+    bannerLogoFigure.addChild(figureImg);
+    bannerLogo.addChild(bannerLogoFigure);
 
-		banner.addChild(bannerLogo);
-		banner.addChild(bannerLogoTitle);
+    banner.addChild(bannerLogo);
+    banner.addChild(bannerLogoTitle);
 
-		bannerNavList.addChild(homeViewItem);
-		bannerNavList.addChild(artistsViewItem);
-		bannerNavList.addChild(albumsViewItem);
-		banner.addChild(bannerNavList);
+    bannerNavList.addChild(homeViewItem);
+    bannerNavList.addChild(artistsViewItem);
+    bannerNavList.addChild(albumsViewItem);
+    banner.addChild(bannerNavList);
 
-		wrapper.addChild(banner);
-	}
+    wrapper.addChild(banner);
+  }
 
-	renderEmptyContentPane() {
-		const wrapper = Html().select(".wrapper");
-		const contentPane = Html()
-			.create("section")
-			.addClass("content");
-		wrapper.addChild(contentPane);
-	}
+  renderEmptyContentPane() {
+    const wrapper = Html().select(".wrapper");
+    const contentPane = Html()
+      .create("section")
+      .addClass("content");
+    wrapper.addChild(contentPane);
+  }
 
-	renderMain() {
-		const contentPane = Html().select(".content");
+  renderMain() {
+    const contentPane = Html().select(".content");
 
-		const main = Html()
-			.create("main")
-			.addClass("main");
+    const main = Html()
+      .create("main")
+      .addClass("main");
 
-		contentPane.addChild(main);
-	}
+    contentPane.addChild(main);
+  }
 
-	renderFooter() {
-		const contentPane = Html().select(".content");
-		const footer = Html()
-			.create("footer")
-			.addClass("footer");
-		const footerContents = Html()
-			.create("h2")
-			.html("&copy;2019 Stonkify Freemium");
+  renderFooter() {
+    const contentPane = Html().select(".content");
+    const footer = Html()
+      .create("footer")
+      .addClass("footer");
+    const footerContents = Html()
+      .create("h2")
+      .html("&copy;2019 Stonkify Freemium");
 
-		footer.addChild(footerContents);
-		contentPane.addChild(footer);
-	}
+    footer.addChild(footerContents);
+    contentPane.addChild(footer);
+  }
 }
--- src/js/Components/ContentBlocks/SingleAlbumBlock.js
+++ src/js/Components/ContentBlocks/SingleAlbumBlock.js
@@ -2,34 +2,121 @@ import Html from "../../Html/Html";
 import Api from "../../Api/Api";
 
 export default class SingleAlbumBlock {
-	constructor(requestedData) {
-		if (requestedData === undefined) {
-			requestedData = "1";
-		} else if (requestedData == "") {
-			requestedData = "1";
-		}
-		this.rootDataURL = Api().getRootURL() + "albums";
-		this.resourceURL = this.rootDataURL + "/" + requestedData;
-		console.log(this.resourceURL);
-	}
+  constructor(requestedData) {
+    if (requestedData === undefined) {
+      requestedData = "1";
+    } else if (requestedData == "") {
+      requestedData = "1";
+    }
+    this.rootDataURL = Api().getRootURL() + "albums";
+    this.resourceURL = this.rootDataURL + "/" + requestedData;
+    console.log(this.resourceURL);
+  }
 
-	renderContent() {
-		const main = Html().select(".main");
-		const content = this.generateContent();
-		main.replace(content);
-	}
+  renderContent() {
+    const main = Html().select(".main");
+    const content = this.generateContent();
+    main.replace(content);
+  }
 
-	generateContent() {
-		const pageTitle = Html()
-			.create("h1")
-			.text("Single Album");
-		const pageSubTitle = Html()
-			.create("h2")
-			.text(this.resourceURL);
-		const content = Html()
-			.create("div")
-			.addChild(pageTitle)
-			.addChild(pageSubTitle);
-		return content;
-	}
+  generateContentOld() {
+    const pageTitle = Html()
+      .create("h1")
+      .text("Single Album");
+    const pageSubTitle = Html()
+      .create("h2")
+      .text(this.resourceURL);
+    const content = Html()
+      .create("div")
+      .addChild(pageTitle)
+      .addChild(pageSubTitle);
+    return content;
+  }
+
+  generateContent() {
+    const pageTitle = Html()
+      .create("h1")
+      .text("Single Album");
+    const pageSubTitle = Html()
+      .create("h2")
+      .text(this.resourceURL);
+    const avatarSection = this.generateAlbumAvatar();
+    const songSection = this.renderSongs();
+    const content = Html()
+      .create("div")
+      .addClass("container")
+      .addChild(pageTitle)
+      .addChild(pageSubTitle)
+      .addChild(avatarSection)
+      .addChild(songSection);
+    return content;
+  }
+
+  generateAlbumAvatar() {
+    const avatarSection = Html()
+      .create("section")
+      .addClass("avatar");
+    const avatarFig = Html()
+      .create("figure")
+      .addClass("avatar__figure");
+    const albumTitle = Html()
+      .create("h1")
+      .addClass("avatar__title");
+
+    Api().getRequest(this.resourceURL, album => {
+      const albumCover = Html()
+        .create("img")
+        .addClass("artist-figure__img")
+        .addAttribute("src", album.imgUrl);
+      const albumTitle = Html()
+        .create("h1")
+        .addClass("avatar__title")
+        .text(album.title);
+      avatarFig.addChild(albumCover);
+      avatarSection.addChild(avatarFig);
+      avatarSection.addChild(albumTitle);
+    });
+    return avatarSection;
+  }
+
+  renderSongs() {
+    const songSection = Html().create("section");
+    Api().getRequest(this.resourceURL, album => {
+      console.log(album.songs);
+      album.songs.forEach(song => {
+        const songBox = Html()
+          .create("div")
+          .addClass("song-box");
+        const playCircle = Html()
+          .create("div")
+          .addClass("play-circle");
+        const playButton = Html()
+          .create("button")
+          .addClass("play-button");
+        const songInfo = Html()
+          .create("div")
+          .addClass("song-info");
+        const songTitle = Html()
+          .create("h3")
+          .text(song.title);
+        const artistName = Html()
+          .create("span")
+          .text("can't get artist name");
+        const songDuration = Html()
+          .create("span")
+          .addClass("duration")
+          .text(song.duration);
+
+        playCircle.addChild(playButton);
+        songBox.addChild(playCircle);
+        songInfo.addChild(songTitle);
+        songInfo.addChild(artistName);
+        songBox.addChild(songInfo);
+        songBox.addChild(songDuration);
+
+        songSection.addChild(songBox);
+      });
+    });
+    return songSection;
+  }
 }
